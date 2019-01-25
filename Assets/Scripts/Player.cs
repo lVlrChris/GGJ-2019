182670f6846ce6a4f2c7670a5f2a14a6bcfdfc9c
Chris <christian-boer@hotmail.com>
2019-01-26 00:07:53 +0100

    Player wip

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed;
     // Start is called before the first frame update
     void Start()
     {
