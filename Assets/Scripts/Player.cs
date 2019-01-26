f3799f8301da7bea98f87a5638b91f404a5c6ac3
Chris <christian-boer@hotmail.com>
2019-01-26 03:48:41 +0100

    Ball movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,15 +6,33 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private float _maxSpinSpeed = 200;
+    [SerializeField]
+    private float _jumpForce;
+
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = _maxSpinSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    // FixedUpdate before physics calculation
+    void FixedUpdate()
     {
-        float horizontalMovement = Inp
+        float xSpeed = Input.GetAxis("Horizontal");
+        float ySpeed = Input.GetAxis("Vertical");
+        
+        xSpeed = xSpeed * _speed * Time.deltaTime;
+        ySpeed = ySpeed * _speed * Time.deltaTime;
+
+        rb.AddTorque(new Vector3(xSpeed, 0, ySpeed));
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        }
     }
 }
