4a1110a8038c3f142a1f862015f07fdc87f5c4c6
Chris <christian-boer@hotmail.com>
2019-01-26 22:41:02 +0100

    Destuctable wip

---
+++ Assets/Scripts/Destructable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _brokenPrefab;
+
+    void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag.Equals("Player")) {
+            Instantiate(_brokenPrefab, transform.position, transform.rotation);
+        }
+    }
+}
