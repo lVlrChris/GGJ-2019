72b444578ce03add3628b065d69d5e82a321a680
Chris <christian-boer@hotmail.com>
2019-01-26 14:01:00 +0100

    Pickup mechanic

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    private Player player;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.transform.tag.Equals("Player")) {
+            player = other.transform.GetComponent<Player>();
+            if (player != null) {
+                player.AddPickup();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
